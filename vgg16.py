c72a56a45f8b72c45920d49e0cbab8e7b4e761b7
Cooper Wakefield <enquiries@cooperwakefield.com>
2019-03-25 11:07:17 +1000

    added layers, made last 3 conv bloacks trainable

--- vgg16.py
+++ vgg16.py
@@ -19,14 +19,23 @@ with open('config.json') as f:
     conf = json.load(f)
 
 vgg16_model = VGG16(weights='imagenet',
-                    input_shape=(conf["height"], conf["width"], 3))
-vgg16_model.layers.pop()
+                    input_shape=(conf["height"], conf["width"], 3),
+                    include_top=False)
+# vgg16_model.layers.pop()
+
+
 model = Sequential()
 for layer in vgg16_model.layers:
     model.add(layer)
-for layer in model.layers:
-    layer.trainable = False
+
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(2, activation='softmax'))
+for layer in model.layers[:-7]:
+    layer.trainable = False
+for layer in model.layers[-7:]:
+    layer.trainable = True
 model.compile(optimizer=optimizers.Adam(lr=0.0001), loss='categorical_crossentropy',
               metrics=['accuracy'])
 datagen = ImageDataGenerator(validation_split=0.2)
@@ -43,5 +52,5 @@ es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=200)
 mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
 model.fit_generator(train_batches, validation_data=valid_batches, 
                     epochs=5, verbose=1)
-model.save_weights('imception_weights.h5')
+model.save_weights('imception_weights_2.h5')
 model.summary()
